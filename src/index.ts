c869b8ad0037d37a752707dad4d46b7315536c98
kbaba1001 <kbaba1001@users.noreply.github.com>
2023-03-05 14:50:51 +0900

    feat: swagger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Application } from "express";
 import morgan from "morgan";
+import Router from "./routes";
+import swaggerUi from "swagger-ui-express";
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,11 +11,17 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
 
-app.get("/ping", async (_req, res) => {
-  res.send({
-    message: "hello",
-  });
-});
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  })
+);
+
+app.use(Router);
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
